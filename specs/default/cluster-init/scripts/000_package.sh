1f4df70085a4be5cbe5a344ca8c88cdd426192ca
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-01-13 12:42:14 +0530

    Changes in running the scripts

--- specs/default/cluster-init/scripts/000_package.sh
+++ specs/default/cluster-init/scripts/000_package.sh
@@ -6,7 +6,7 @@
 #Installing the required Packages
 
 OS_VER=$(jetpack config platform_family)
-case $OS_VER in
+case $OS_VER in 
 rhel)
     yum clean all
     yum install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python-utils -y
@@ -15,6 +15,7 @@ debian)
     export DEBIAN_FRONTEND=noninteractive
     apt-get update
     sudo -E apt -y -qq install sssd realmd oddjob oddjob-mkhomedir adcli samba-common krb5-user sssd-krb5 ldap-utils policycoreutils-python-utils sssd-tools sssd libnss-sss libpam-sss adcli packagekit -y
+    sleep 300
     echo "session required pam_mkhomedir.so" >> /etc/pam.d/common-session
     ;;
 esac
