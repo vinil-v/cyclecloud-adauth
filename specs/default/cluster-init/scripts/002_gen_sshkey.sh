87d1c4978258db1da5fd807e085915332dd42c01
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-01-31 11:00:40 +0530

    Create 002_gen_sshkey.sh

---
+++ specs/default/cluster-init/scripts/002_gen_sshkey.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#automating SSH key creation and passwordless configuration
+cat <<EOF >/etc/profile.d/gen_sshkey.sh
+#!/bin/sh
+if [ ! -f  ~/.ssh/id_rsa.pub ] ; then
+        ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+fi
+EOF
+chmod +x /etc/profile.d/gen_sshkey.sh
