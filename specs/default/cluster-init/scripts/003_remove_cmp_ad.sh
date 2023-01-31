b8f64f0b7210794e9215b2bea82f04120e4b5b12
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-01-31 20:34:53 +0530

    Update 003_remove_cmp_ad.sh

--- specs/default/cluster-init/scripts/003_remove_cmp_ad.sh
+++ specs/default/cluster-init/scripts/003_remove_cmp_ad.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-#cleanup computer resources from Activate directory after node terminatation. 
-cat <<EOF > /opt/realm_cleanup.sh
-AD_SERVER=$(jetpack config adauth.ad_server)
-AD_SERVER_IP=$(jetpack config adauth.ad_server_ip)
-AD_DOMAIN=$AD_SERVER
-ADMIN_NAME=$(jetpack config adauth.ad_admin_user)
-ADMIN_PASSWORD=$(jetpack config adauth.ad_admin_password)
+#cleanup computer resources from Activate directory after node terminatation.
+cat <<\EOF > /opt/realm_cleanup.sh
+#!/bin/sh
+ADMIN_NAME=$(/opt/cycle/jetpack/bin/jetpack config adauth.ad_admin_user)
+ADMIN_PASSWORD=$(/opt/cycle/jetpack/bin/jetpack config adauth.ad_admin_password)
 
 echo $ADMIN_PASSWORD | realm leave -U $ADMIN_NAME  --remove
 EOF
@@ -28,5 +26,3 @@ WantedBy=shutdown.target
 EOF
 systemctl daemon-reload
 systemctl enable realmcleanup-before-shutdown.service
-systemctl start realmcleanup-before-shutdown.service
-
