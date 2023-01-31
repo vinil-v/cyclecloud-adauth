0f11ec047e1c7020d3e9e97edd28c7134c7373d6
Vinil Vadakkepurakkal <vinilv@microsoft.com>
2023-01-31 11:13:25 +0530

    Update 002_gen_sshkey.sh

--- specs/default/cluster-init/scripts/002_gen_sshkey.sh
+++ specs/default/cluster-init/scripts/002_gen_sshkey.sh
@@ -5,6 +5,7 @@ cat <<EOF >/etc/profile.d/gen_sshkey.sh
 if [ ! -f  ~/.ssh/id_rsa.pub ] ; then
         ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
         cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+        chmod 644 ~/.ssh/authorized_keys
 fi
 EOF
 chmod +x /etc/profile.d/gen_sshkey.sh
